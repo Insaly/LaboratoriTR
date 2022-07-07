ffff1aa13f4bc4f03c387f1083c7d221fff21702
Insaly <lluc@aymerich.com>
2022-07-07 12:55:09 +0200

    v.0.0.3

--- Projecte/Entitats/Generador_slimes.gd
+++ Projecte/Entitats/Generador_slimes.gd
@@ -2,17 +2,11 @@ extends Area2D
 
 const Slime = preload("res://Entitats/Slime.tscn")
 
-func _process(delta):
-#	while 1 < 2:
-#		$Timer_generador.start()
-	if Input.is_action_pressed("ui_accept"):
-		genera()
-		
-func _on_Timer_generador_timeout():
-	var Slime = Slime.instance()
-	Slime.position = position
-	get_parent().add_child(Slime)
-func genera():
-	var slime = Slime.instance()
-	Slime.position = position
-	get_parent().add_child(slime) 
+func _ready():
+	$Generador.start()
+
+func _on_Generador_timeout():
+	var Subjecte = Slime.instance()
+	add_child(Subjecte)
+	Subjecte.position = $Position2D.position
+	$Generador.start()
