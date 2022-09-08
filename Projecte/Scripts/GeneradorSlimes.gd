283d0d8ea47f8f6a1386cb6511e0c04135ffc083
Insaly <lluc@aymerich.com>
2022-09-09 00:40:13 +0200

    v.0.1.2

--- Projecte/Scripts/GeneradorSlimes.gd
+++ Projecte/Scripts/GeneradorSlimes.gd
@@ -4,14 +4,6 @@ const Slime = preload("res://Entitats/Slime.tscn")
 var nombre_entitats = 3000
 var entitats_generades = 0
 
-#func _ready():
-#	while nombre_entitats > entitats_generades:
-#		entitats_generades += 1
-#		print(entitats_generades)
-#		var Subjecte = Slime.instance()
-#		add_child(Subjecte)
-#		Subjecte.position = $Position2D.position
-		
 func _process(delta):
 	if nombre_entitats > entitats_generades:
 		entitats_generades += 1
--- Projecte/Scripts/Sala01.gd
+++ Projecte/Scripts/Sala01.gd
@@ -1,8 +1,36 @@
 extends Node2D
 
-func _ready():
-	pass # Replace with function body.
+var TileMap01 = preload("res://Entitats/TileMap01.tscn")
+var TileMap02 = preload("res://Entitats/TileMap02.tscn")
+var TileMap03 = preload("res://Entitats/TileMap03.tscn")
+var TileMap04 = preload("res://Entitats/TileMap04.tscn")
+
+var Activat01 = true
+var Activat02 = true
+var Activat03 = true
+var Activat04 = true
 
 func _process(delta):
 	if Input.is_action_just_pressed("1"):
-		print("ACTIVAT")
+		if Activat01:
+			var TileMap01Entity = TileMap01.instance()
+			$Position2D.add_child(TileMap01Entity)
+		Activat01 = !Activat01
+		
+	if Input.is_action_just_pressed("2"):
+		if Activat02:
+			var TileMap02Entity = TileMap02.instance()
+			$Position2D.add_child(TileMap02Entity)
+		Activat02 = !Activat02
+		
+	if Input.is_action_just_pressed("3"):
+		if Activat03:
+			var TileMap03Entity = TileMap03.instance()
+			$Position2D.add_child(TileMap03Entity)
+		Activat03 = !Activat03
+		
+	if Input.is_action_just_pressed("4"):
+		if Activat04:
+			var TileMap04Entity = TileMap04.instance()
+			$Position2D.add_child(TileMap04Entity)
+		Activat04 = !Activat04	
